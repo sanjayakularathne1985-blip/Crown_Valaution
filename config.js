c47e68f35ac45cd499fa63852b27261bb554c803
sanjayakularathne1985-blip <sanjayakularathne1985@gmail.com>
2026-06-14 21:25:39 +0530

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxQnG15Y2Vi_55sfOnHJai8tVVwMDsDSLDvA5kwEQa3e-rCaeoKohmPjTiP_fAA2lJS/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyzrtT5uB3pfKF9hO9cmFbFBKOJdKS9BZ0r_vypG_p2Jyx-fOsMtn3e4slaXnvS4qFz/exec";
